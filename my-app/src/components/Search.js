6b941fe6de636b31e40a1f7929a2758ab41b67d0
danielgreg2 <gdecanio@ufl.edu>
2019-10-30 13:13:28 -0400

    Added comments further explaining how Search.js code works

--- my-app/src/components/Search.js
+++ my-app/src/components/Search.js
@@ -4,7 +4,7 @@ class Search extends React.Component {
 	filterUpdate() {
 		//Here you will need to update the value of the filter with the value from the textbox
 		const val = this.myValue.value      //get what is typed in the box
-		console.log(val)		//will output to console what is in the search box every time it gets updated
+		//console.log(val)					//will output to console what is in the search box every time it gets updated
 		this.props.filterUpdate(val)        //this is referring to the 'filterUpdate' in App.js
 	}
 	render() {
@@ -16,8 +16,9 @@ class Search extends React.Component {
 				<input
 					type="text" 
 					placeholder="Type to Filter" 
-					ref = {(value) => {this.myValue = value}}
-                	onChange = {this.filterUpdate.bind(this)}	//every time the value inside the filter changes, filterUpdate is called
+					ref = {(value) => {this.myValue = value}}	//grabs the value from the textbox
+					onChange = {this.filterUpdate.bind(this)}	//every time the value inside the filter changes, filterUpdate is called
+																//this saves the value in the textbox, also passing it back to App.js
 				/>
 			</form>
 		);
